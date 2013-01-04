e943745fe6e7c5c1ba781d32b4fa0038690a8731
David Volm <david@volminator.com>
2013-01-03 19:57:21 -0600

    v0.0.1

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ cs-stringjs
 ===========
 
 coffeeshop.js repo for string.js
+
+Install
+------
+```bash
+npm install https://github.com/daxxog/cs-stringjs/tarball/master
+```
---
+++ cs_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#npm module for coffeeshop.js installer
+mkdir cs_serve
+cd cs_serve
+wget https://raw.github.com/jprichardson/string.js/master/lib/string.min.js
+wget https://raw.github.com/jprichardson/string.js/master/lib/string.js
